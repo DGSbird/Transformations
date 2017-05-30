5b36661f61a3a79c374a5b541bb99bffaec9082d
Lin, Dominik <lindomi@ecbt1.tadnet.net>
2017-05-30 19:31:37 +0200

    > feat: transformation context

---
+++ src/main/java/ecb/transformations/BirdVtl/TContext.java
@@ -0,0 +1,13 @@
+package ecb.transformations.BirdVtl;
+
+/**
+ * Transformation context.
+ * 
+ * @author Dominik Lin
+ *
+ */
+public class TContext {
+    final public static String origin = "origin";
+
+    final public static String eof = "<EOF>";
+}
