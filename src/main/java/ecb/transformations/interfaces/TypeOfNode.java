189fce3257b6df75acf86afc67736451e8c7530a
Lin, Dominik <lindomi@ecbt1.tadnet.net>
2017-05-30 19:31:25 +0200

    > feat: type of node interface

---
+++ src/main/java/ecb/transformations/interfaces/TypeOfNode.java
@@ -0,0 +1,65 @@
+package ecb.transformations.interfaces;
+
+import java.io.Serializable;
+
+import ecb.transformations.enums.Bracket;
+
+/**
+ * Type of node interface comprising methods that need to be implemented by all
+ * {@link Enum}s representing nodes.
+ * 
+ * @author Dominik Lin
+ *
+ */
+public interface TypeOfNode extends Serializable {
+    /**
+     * 
+     * @return the {@link String} representation of this type of node (e.g.
+     *         ":=")
+     */
+    public String getTypeOfNode();
+
+    /**
+     * 
+     * @return the {@link Bracket} related to this type of node (e.g.
+     *         {@link Bracket#ROUND}
+     */
+    default public Bracket getBracket() {
+	return Bracket.NONE;
+    };
+
+    /**
+     * 
+     * @return the separator between this node's children
+     */
+    default public String getSeparator() {
+	return " ";
+    };
+
+    /**
+     * 
+     * @return the separator between this node's expression (i.e. the operator)
+     *         and it's children (i.e. the operands)
+     */
+    default public String getOperatorChildrenSeparator() {
+	return ", ";
+    };
+
+    /**
+     * 
+     * @return the end of line symbol (e.g. ";")
+     */
+    default public String getEOL() {
+	return "";
+    };
+
+    /**
+     * 
+     * @return the alias of this node's expression (e.g. "define procedure " for
+     *         "Procedure information" <code>node</code>)
+     */
+    default public String getAlias() {
+	return "";
+    };
+
+}
