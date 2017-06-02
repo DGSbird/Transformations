53e02b304185eaf9aa11ad3c0bdd43ba5a6f1b8a
Lin, Dominik <lindomi@ecbt1.tadnet.net>
2017-06-03 00:17:10 +0200

    > docs: added description to methods

--- src/main/java/ecb/transformations/interfaces/components/WebComponent.java
+++ src/main/java/ecb/transformations/interfaces/components/WebComponent.java
@@ -1,16 +1,22 @@
 package ecb.transformations.interfaces.components;
 
-import java.net.URL;
-
 /**
  * Extends the {@link Component} interface with methods related to the
- * illustration on the web site (i.e. get / set methods for link and tooltip).
+ * illustration on the web site (i.e. get / set methods for link and tool tip).
  * 
  * @author Dominik Lin
  *
  */
 public interface WebComponent extends Component {
-    public URL getUniformResourceLocation();
+    /**
+     * 
+     * @return the (web) location of this object
+     */
+    public String getLocation();
 
+    /**
+     * 
+     * @return the tool tip related to this object
+     */
     public String getTooltip();
 }
