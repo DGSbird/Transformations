eef9b6fd1de35fe3a5d7802fee7a9340182c277f
Lin, Dominik <lindomi@ecbt1.tadnet.net>
2017-06-02 19:55:34 +0200

    > feat: jpa annotation for fields of transformation component

--- src/main/java/ecb/transformations/treeStructure/TComponent.java
+++ src/main/java/ecb/transformations/treeStructure/TComponent.java
@@ -1,35 +1,117 @@
-package ecb.transformations.BirdVtl;
+package ecb.transformations.treeStructure;
 
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import ecb.transformations.interfaces.TypeOfNode;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import ecb.transformations.interfaces.Similar;
 import ecb.transformations.interfaces.WebComponent;
-
 
 /**
  * Stand alone transformation component without consideration of the tree / node
  * structure of the transformation. This class acts as a data field for
- * {@link Node} respectively {@link TNode} objects.
+ * {@link Node} respectively {@link TNode} objects.<br>
+ * Please note that the identifier for this class is the {@link #componentId}
+ * which also acts as the relevant input for {@link #equals(Object)} and
+ * {@link #hashCode()}. Similar {@link TComponent}s (i.e. components having the
+ * same expression & type) are identified using {@link #isSimilar(Object)}.
  * 
  * @author Dominik Lin
  *
  */
-public class TComponent implements WebComponent, Serializable {
+@Entity
+@SuppressWarnings("rawtypes")
+public class TComponent implements Similar, WebComponent, Serializable {
     // ----------------------------------------------------------
     // fields
     // ----------------------------------------------------------
 
     private static final long serialVersionUID = -943562954113189135L;
 
-    protected String expression;
+    /**
+     * The (technical) identifier of this {@link TComponent}
+     */
+    @Id
+    @Column(name = "componentId", nullable = false)
+    private int componentId;
 
-    protected TypeOfNode type;
+    /**
+     * The expression of this {@link TComponent} (e.g. result)
+     */
+    @Column(name = "expression", length = 100000)
+    private String expression;
 
-    protected String comment = "";
+    /**
+     * The type of this {@link TComponent} (e.g. CUBE_ID)
+     */
+    @Column(name = "type")
+    private String type;
 
-    protected String link = "";
+    /**
+     * The comment related to this {@link TComponent}
+     */
+    @Column(name = "comment", length = 100000)
+    private String comment = "";
 
-    protected String tooltip = "";
+    /**
+     * The {@link URL} related to this {@link TComponent}
+     */
+    @Column(name = "uniformResourceLocation")
+    private URL uniformResourceLocation;
+
+    /**
+     * The tool tip related to this {@link TComponent}
+     */
+    @Column(name = "tooltip")
+    private String tooltip = "";
+
+    /**
+     * The description related to this {@link TComponent}
+     */
+    @Column(name = "description", length = 100000)
+    private String description;
+
+    /**
+     * The {@link TNode} related to this {@link TComponent}
+     */
+    @OneToOne
+    private TNode node;
+
+    // ----------------------------------------------------------
+    // equals, hashCode, isSimilar
+    // ----------------------------------------------------------
+
+    @Override
+    public boolean equals(Object obj) {
+	boolean equals = false;
+	if (obj instanceof TComponent) {
+	    TComponent component = (TComponent) obj;
+	    equals = this.componentId == component.componentId;
+	}
+	return equals;
+    }
+
+    @Override
+    public int hashCode() {
+	return new HashCodeBuilder().append(componentId).append(getClass()).hashCode();
+    }
+
+    @Override
+    public boolean isSimilar(Object obj) {
+	boolean isSimilar = false;
+	if (obj instanceof TComponent) {
+	    TComponent component = (TComponent) obj;
+	    isSimilar = this.expression.equals(component.expression) && this.type.equals(component.type);
+	}
+	return isSimilar;
+    }
 
     // ----------------------------------------------------------
     // constructor
@@ -43,12 +125,12 @@ public class TComponent implements WebComponent, Serializable {
 	setExpression(expression);
     }
 
-    public TComponent(String expression, TypeOfNode type) {
+    public TComponent(String expression, String type) {
 	setExpression(expression);
 	setType(type);
     }
 
-    public TComponent(String expression, TypeOfNode type, String comment) {
+    public TComponent(String expression, String type, String comment) {
 	setExpression(expression);
 	setType(type);
 	setComment(comment);
@@ -64,7 +146,7 @@ public class TComponent implements WebComponent, Serializable {
     }
 
     @Override
-    public TypeOfNode getType() {
+    public String getType() {
 	return type;
     }
 
@@ -74,8 +156,8 @@ public class TComponent implements WebComponent, Serializable {
     }
 
     @Override
-    public String getLink() {
-	return link;
+    public URL getUniformResourceLocation() {
+	return uniformResourceLocation;
     }
 
     @Override
@@ -83,9 +165,22 @@ public class TComponent implements WebComponent, Serializable {
 	return tooltip;
     }
 
+    @Override
+    public String getDescription() {
+	return description;
+    }
+
     // ----------------------------------------------------------
     // additional methods
     // ----------------------------------------------------------
+
+    public int getComponentId() {
+	return componentId;
+    }
+
+    public void setComponentId(int componentId) {
+	this.componentId = componentId;
+    }
 
     public void setTooltip(String tooltip) {
 	this.tooltip = tooltip;
@@ -99,12 +194,51 @@ public class TComponent implements WebComponent, Serializable {
 	this.comment = comment;
     }
 
-    public void setType(TypeOfNode type) {
+    public void setType(String type) {
 	this.type = type;
     }
 
-    public void setLink(String link) {
-	this.link = link;
+    public void setUniformResourceLocation(URL uniformResourceLocation) {
+	this.uniformResourceLocation = uniformResourceLocation;
+    }
+
+    /**
+     * Allows to set the {@link URL} of this {@link TComponent} using the string
+     * representation of the URL. This method throws a
+     * <tt>MalformedURLException</tt>.
+     * 
+     * @param uniformResourceLocator
+     *            the {@link String} representation of the Uniform Resource
+     *            Locator
+     * @exception MalformedURLException
+     *                if the given input does not comply with the Uniform
+     *                Resource Locator syntax
+     */
+    public void setUniformResourceLocation(String uniformResourceLocation) {
+	try {
+	    this.uniformResourceLocation = new URL(uniformResourceLocation);
+	} catch (MalformedURLException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public void setDescription(String description) {
+	this.description = description;
+    }
+
+    public TNode getNode() {
+	return node;
+    }
+
+    public void setNode(TNode node) {
+	setNode(node, true);
+    }
+
+    public void setNode(TNode node, boolean set) {
+	this.node = node;
+	if (node != null && set) {
+	    node.setComponent(this, false);
+	}
     }
 
     // ----------------------------------------------------------
