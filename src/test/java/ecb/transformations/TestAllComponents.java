e4dbfe11132928a3aacfee435e70adfcdffd7572
Lin, Dominik <lindomi@ecbt1.tadnet.net>
2017-06-26 21:41:01 +0200

    > test: suite for all test classes

---
+++ src/test/java/ecb/transformations/TestAllComponents.java
@@ -0,0 +1,11 @@
+package ecb.transformations;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestTComponent.class, TestTNode.class, TestTScheme.class, TestTTree.class })
+public class TestAllComponents {
+
+}
